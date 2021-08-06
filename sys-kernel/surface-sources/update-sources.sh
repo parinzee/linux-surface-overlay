fc8a17b4b645b69e5fb510b80eae1b68a809018b
Parinthapat P <parinzee@protonmail.com>
2021-08-06 15:47:46 +0700

    Changed from ssh to https for cloning

--- sys-kernel/surface-sources/update-sources.sh
+++ sys-kernel/surface-sources/update-sources.sh
@@ -7,7 +7,7 @@ echo "Cloning in Linux Surface Repo..."
 mkdir linux-surface
 cd linux-surface
 git init
-git remote add -f origin git@github.com:linux-surface/linux-surface.git
+git remote add -f origin https://github.com/linux-surface/linux-surface.git
 git config core.sparseCheckout true
 echo "patches/" >> .git/info/sparse-checkout
 git pull origin master
