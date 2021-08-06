6e33cc75f34045e7f5c61503aea826697cd096fd
Parinz <parinzee@protonmail.com>
2021-08-06 22:31:00 +0700

    Updated patches

--- sys-kernel/surface-sources/update-sources.sh
+++ sys-kernel/surface-sources/update-sources.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Removing existing patches"
+rm -rf files/*
+
 echo "Cloning in Linux Surface Repo..."
 mkdir linux-surface
 cd linux-surface
@@ -10,9 +13,11 @@ echo "patches/" >> .git/info/sparse-checkout
 git pull origin master
 
 echo "Moving Patches..."
-mv patches/* ../files/
+cd patches
+rm -rf 5.2 5.3 5.4 5.5 5.6 5.7 5.8 5.9 5.10
+mv * ../../files/
 
 echo "Deleting cloned repo"
-cd ..
+cd ../..
 rm -rf linux-surface
 echo "Done! Patches are updated!"
